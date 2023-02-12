4d72dbeea7c55053434681826dccb547004ccd38
levkovich-polina <levkovich.polina@gmail.com>
2023-02-12 18:56:34 +0300

    Painted clouds

--- Task5/Task5/Form1.cs
+++ Task5/Task5/Form1.cs
@@ -32,13 +32,22 @@ namespace Task5
                 Brush = brush;
             }
         }
-     
+
         List<Drop> _drops = new List<Drop>();
         List<Cloud> _clouds = new List<Cloud>();
         Timer _timer;
         Random _random = new Random();
         int _positionX;
         SolidBrush _brush;
+        List<int> _listPositionX = new List<int>();
+
+        int _width;
+        int _height;
+        int _dx;
+        int _radius;
+        int _dy;
+        int _dropX;
+        int _dropY;
 
         public Form1()
         {
@@ -53,46 +62,55 @@ namespace Task5
             }
             else
             {
+                for (int j = 2; j < 28; j++)
+                {
+                    int chance = _random.Next(1, 11);
+                    if (chance > 1)
+                    {
+                        _listPositionX.Add(j);
+                    }
+                }
+                _width = Panel.ClientSize.Width / 30;
+                _height = Panel.ClientSize.Height / 30;
+                _radius = 35;
+                _dy = _height * 4;
+                _dropY = _height * 10;
+
                 TimerCallback tm = new TimerCallback(OnTimerTicked);
-                _timer = new Timer(tm, 0, 0, 100);
+                _timer = new Timer(tm, 0, 0, 1000);
             }
         }
 
         private void OnTimerTicked(object obj)
         {
-            _positionX = _random.Next(1, 30);
+            _positionX = _random.Next(2, 29);
+            _dropX = _width * _positionX;
             Draw();
         }
-
         private void Draw()
         {
             Graphics g = Panel.CreateGraphics();
-            int width = Panel.ClientSize.Width / 30;
-            int height = Panel.ClientSize.Height / 30;
-            int dy = height * 4;
-            int radius = 40;
-            int dx = width * 2;
-            for (int i = 0; i < 2; i++)
+            for (int j = 0; j < _listPositionX.Count; j++)
             {
-                for (int j = 0; j < 7; j++)
+                _dx = _width * _listPositionX[j];
+                Cloud cloud = new Cloud(new Point(_dx - _radius, _dy - _radius), _radius, _brush);
+                _clouds.Add(cloud);
+                Invoke(() =>
                 {
-                    Cloud cloud = new Cloud(new Point(dx - radius, dy - radius), radius, _brush);
-                    _clouds.Add(cloud);
-                    Invoke(() =>
-                    {
-                        g.FillEllipse(_brush, dx - radius, dy - radius, radius * 2, radius * 2);
-                    });
-                    dx += width * 2;
-                }
-                dx += width;
+                    g.FillEllipse(_brush, _dx - _radius, _dy - _radius, _radius * 2, _radius * 2);
+                });
             }
+
+            Drop drop = new Drop(new Point(_dropX - _radius, _dropY - _radius), 6, 10, _brush);
+            _drops.Add(drop);
+            g.FillEllipse(_brush, _dropX - _radius, _dropY - _radius, 6, 10);
+            _dropY += _height;
         }
 
         private void ColorButton_Click(object sender, EventArgs e)
-        {          
+        {
             if (ColorDialog.ShowDialog() == DialogResult.OK)
             {
-                Panel.CreateGraphics().Clear(Color.White);
                 _brush = new SolidBrush(ColorDialog.Color);
                 ColorButton.BackColor = ColorDialog.Color;
             }
